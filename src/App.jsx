8ea770872e9659d943bf84c7e231af8c1729b89c
Aidan Fournier <aidan.douglas.fournier@gmail.com>
2023-06-17 00:03:07 +0900

    Put CTA button in reusable component

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import ReactCardCarousel from "react-card-carousel";
 
 import styles from './styles/Global';
+import Button from './components/Button';
 import { AttractionsIcon, HeroTori, HotelsIcon, RestaurantsIcon, ExpoIcon, GitHubIcon, QRCode } from "./assets";
 
 const App = () => {
@@ -66,18 +67,10 @@ const App = () => {
             </h3>
           </div>
 
-          <div className="mt-[1rem] flex justify-center items-center">
-            <button 
-              onClick={handleClick}
-              className='text-white bg-gradient-to-r from-[#5CA7F1] to-[#336699] hover:bg-gradient-to-bl px-8 py-4 rounded-2xl text-center hover:drop-shadow-2xl transition-all scale-100 hover:scale-105 ease-in duration-300'
-            >
-              {isShown ? 
-                <h1 className='text-white text-xl drop-shadow-xl'>Hide QR Code</h1>
-                :
-                <h1 className='text-white text-xl drop-shadow-xl'>Reveal QR Code</h1>
-              }
-            </button>
-          </div>
+          <Button 
+            handleClick={handleClick}
+            showQR={isShown}
+          />
 
           <div className="mt-[5rem] flex justify-center items-center space-x-3 ">
             <div 
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,16 +1,19 @@
 import styles from '../styles/Global';
 
-const Button = ({ assetUrl, link }) => {
+// eslint-disable-next-line react/prop-types
+const Button = ({ handleClick, showQR }) => {
   return (
-    <div
-      className={`${styles.btnBlack} hover:shadow-lg`}
-      onClick={() => window.open(link, "_blank")}
-    >
-      <img src={assetUrl} alt="expo-icon" className={styles.btnIcon} />
-      <div className="flex flex-col justify-start ml-4">
-        <p className={`${styles.btnText} font-normal text-xs`}>View it on</p>
-        <p className={`${styles.btnText} font-bold text-sm`}>Expo Store</p>
-      </div>
+    <div className="mt-[1rem] flex justify-center items-center">
+      <button 
+        onClick={handleClick}
+        className='text-white bg-gradient-to-r from-[#5CA7F1] to-[#336699] hover:bg-gradient-to-bl px-8 py-4 rounded-2xl text-center hover:drop-shadow-2xl transition-all scale-100 hover:scale-105 ease-in duration-300'
+      >
+        {showQR ? 
+          <h1 className='text-white text-xl drop-shadow-xl'>Hide QR Code</h1>
+          :
+          <h1 className='text-white text-xl drop-shadow-xl'>Reveal QR Code</h1>
+        }
+      </button>
     </div>
   )
 }
