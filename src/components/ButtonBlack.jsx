14e056ca0aebe554a718e17a96054781b18dbe3d
Aidan Fournier <aidan.douglas.fournier@gmail.com>
2023-08-02 23:27:36 +0900

    Fixed issue with external links

--- src/components/ButtonBlack.jsx
+++ src/components/ButtonBlack.jsx
@@ -3,16 +3,20 @@ import styles from '../styles/Global';
 // eslint-disable-next-line react/prop-types
 const ButtonBlack = ({ url, icon, alt, title }) => {
   return (
-    <div 
-        className={`${styles.btnBlack}`}
-        onClick={() => window.open({url}, "_blank")}
+    <a 
+      href={url}
+      target='_blank'
+      rel="noreferrer"
+      aria-label={alt}
+      className={`${styles.btnBlack}`}
+      // onClick={() => window.open({url}, "_blank")}
     >
         <img src={icon} alt={alt} className={styles.btnIcon} />
         <div className="flex flex-col justify-start lg:ml-4 ml-2">
             <p className={`${styles.btnText}`}>View it on</p>
             <p className={`${styles.btnTextBold}`}>{title}</p>
         </div>
-    </div>
+    </a>
   )
 }
 
